b38d214465ba0ac919cf3a62181e0f2009e4b357
Alexandru Girtan <alexandru.a.girtan@gmail.com>
2024-01-21 18:14:33 +0100

    Update 21/01

--- src/main/java/com/example/studentsmanager/repository/StudentRepository.java
+++ src/main/java/com/example/studentsmanager/repository/StudentRepository.java
@@ -2,6 +2,12 @@ package com.example.studentsmanager.repository;
 
 import com.example.studentsmanager.model.StudentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface StudentRepository extends JpaRepository<StudentModel,Long> {
+    void deleteStudentById(Long id);
+
+    Optional<StudentModel> findStudentById(Long id);
 }
--- src/main/java/com/example/studentsmanager/service/StudentService.java
+++ src/main/java/com/example/studentsmanager/service/StudentService.java
@@ -1,10 +1,12 @@
 package com.example.studentsmanager.service;
 
+import com.example.studentsmanager.exception.UserNotFound;
 import com.example.studentsmanager.model.StudentModel;
 import com.example.studentsmanager.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -13,11 +15,29 @@ public class StudentService {
     private final StudentRepository studentRepository;
     @Autowired
     public StudentService(StudentRepository studentRepository) {
+
         this.studentRepository = studentRepository;
     }
 
     public StudentModel addStudent(StudentModel studentModel){
         studentModel.setStudentCode(UUID.randomUUID().toString());
         return studentRepository.save(studentModel);
+    }
+
+    public List<StudentModel> findAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    public StudentModel updateStudent(StudentModel studentModel){
+        return studentRepository.save(studentModel);
+    }
+
+    public StudentModel findStudentById(Long id){
+        return studentRepository.findStudentById(id)
+                        .orElseThrow(()->new UserNotFound("User by id" + id+"was not found!"));
+    }
+
+    public void deleteStudent(Long id){
+        studentRepository.deleteStudentById(id);
     }
 }
